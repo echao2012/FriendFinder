5d81d98b7411ae115ab9f77f185fa0540a6548b4
echao2012 <47439716+echao2012@users.noreply.github.com>
2019-04-07 00:49:12 -0500

    Added calculation to find closest friend

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,20 +2,37 @@
 const express = require("express");
 const path = require("path");
 
+// Include the friends data
+const friends = require(path.join(__dirname, "..", "data", "friends"))
+
 // Setup express router
 const router = express.Router();
 
 // Route to friends JSON
 router.get("/api/friends", function(req, res) {
-    return res.json();
+    return res.json(friends);
 });
 
-// Route to survey page
+// Route to add friends data
 router.post("/api/friends", function(req, res) {
     const newFriend = req.body;
     console.log(newFriend);
 
-    res.json(newFriend);
+    let iMin = 0;
+    let minScore;
+
+    friends.forEach(function(friend, iFriends) {
+        // Calculate the total difference
+        const score = friend.scores.reduce((acc, val, i) => acc + Math.abs(val - newFriend.scores[i]), 0);
+
+        // Check if this is the closest match
+        if(minScore > score || minScore === undefined) {
+            iMin = iFriends;
+            minScore = score;
+        }
+    });
+
+    res.json(friends[iMin]);
 });
 
 module.exports = router;
