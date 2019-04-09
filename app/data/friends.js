a582d3d435d836c0818f75065562b6d57af4eed8
echao2012 <47439716+echao2012@users.noreply.github.com>
2019-04-08 21:06:28 -0500

    Changed alert to modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 const friends = [{
     "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"https://avatars2.githubusercontent.com/u/5855843?s=460&v=4",
     "scores":[
         5,
         1,
@@ -16,18 +16,18 @@ const friends = [{
 },
 {
     "name":"Eric",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"https://avatars0.githubusercontent.com/u/47439716?s=460&v=4",
     "scores":[
         3,
+        4,
+        1,
         3,
         3,
         3,
+        2,
         3,
-        3,
-        3,
-        3,
-        3,
-        3
+        2,
+        1
     ]
 }];
 
